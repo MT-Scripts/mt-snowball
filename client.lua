5a95c9353fabced8f695b6a20b5d43e0eb3db91d
Marttins <89866234+Marttins011@users.noreply.github.com>
2022-11-17 00:56:01 +0000

    Added option to be snowing whenever script restart

--- client.lua
+++ client.lua
@@ -1,3 +1,11 @@
+AddEventHandler('onResourceStart', function(resource)
+    if resource == GetCurrentResourceName() then
+        if Config.UseStaticXMASWeather == true then
+            TriggerServerEvent('qb-weathersync:server:setWeather', 'XMAS')
+        end
+    end
+end)
+
 function PickSnowBall()
     if IsNextWeatherType('XMAS') and not IsPedInAnyVehicle(GetPlayerPed(-1), true) and not IsPlayerFreeAiming(PlayerId()) and not IsPedSwimming(PlayerPedId()) and not IsPedSwimmingUnderWater(PlayerPedId()) and not IsPedRagdoll(PlayerPedId()) and not IsPedFalling(PlayerPedId()) and not IsPedRunning(PlayerPedId()) and not IsPedSprinting(PlayerPedId()) and GetInteriorFromEntity(PlayerPedId()) == 0 and not IsPedShooting(PlayerPedId()) and not IsPedUsingAnyScenario(PlayerPedId()) and not IsPedInCover(PlayerPedId(), 0) then
         TaskPlayAnim(PlayerPedId(), 'anim@mp_snowball', 'pickup_snowball', 8.0, -1, -1, 0, 1, 0, 0, 0)
@@ -7,8 +15,7 @@ function PickSnowBall()
 end
 
 CreateThread(function()
-    local weather = IsNextWeatherType('XMAS')
-    while weather do
+    while true do
         if IsControlJustReleased(0, Config.Key) then
             PickSnowBall()
         end
--- config.lua
+++ config.lua
@@ -2,3 +2,4 @@ Config = Config or {}
 
 Config.SnowBallsAmount = 2 -- How many snowballs you get in only 1 pick
 Config.Key = 119 -- The button that you need to press to pick balls (default as [E])
+Config.UseStaticXMASWeather = false -- If true it will be snowing whenever script restarts (need to have qb-weathersync!!) (not a standalone option!!)
